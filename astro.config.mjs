7241a79697749354c696550505091e41d5b152d3
lassespilling <lassespilling@gmail.com>
2024-12-09 12:12:56 +0100

    change env method

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
@@ -9,4 +9,16 @@ export default defineConfig({
   integrations: [react()],
   output: 'server',
   adapter: cloudflare({ imageService: 'passthrough' }), // Use custom image service
+  env: {
+    schema: {
+      GITHUB_REPO: envField.string({ context: 'client', access: 'public' }),
+      CLOUDFLARE_DASH: envField.string({ context: 'client', access: 'public' }),
+      NEW_AKSELL_TOOL: envField.string({ context: 'client', access: 'public' }),
+      EDIT_HTML: envField.string({ context: 'client', access: 'public' }),
+      EDIT_LINKS: envField.string({ context: 'client', access: 'public' }),
+      EDIT_MARKDOWN: envField.string({ context: 'client', access: 'public' }),
+      EDIT_PENS: envField.string({ context: 'client', access: 'public' }),
+      API_SECRET: envField.string({ context: 'server', access: 'secret' }),
+    },
+  },
 });
